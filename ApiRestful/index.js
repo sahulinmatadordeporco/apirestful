fe1503620f75b12e026feb4929f225435fd01978
THOMAS VIEIRA CLEMENTE <thomas_clemente@estudante.sesisenai.org.br>
2025-04-30 08:47:53 -0300

    código novo

--- ApiRestful/index.js
+++ ApiRestful/index.js
@@ -1,74 +1,83 @@
-const express =require('express') //importar modulo express do npm
+const express = require('express')// importar modulo express do npm
+const app = express()// inicializar o servidor express e salvar variavel app
+const fs = require('fs')
+const PORT = 8000 // separa uma porta para rodar o servidor
+app.use(express.json())//fala para o servidor que vai receber dados em JSON
 
-const app = express() //inicializa o servidor express e salva ma variavel app 
-const PORT = 8000//separa uma porta para rodar o servidor
-
-const bancoDeDados = [
+const bancodeDados = [
     {
-        id: 1,
-        titulo: "desenvolvimento de sistemas",
-        curso:"tecnico em desenvolvimento",
-        turma: "3b",
-        professor: "Ramon",
+        id:1,
+        titulo:"desenvolvimento de sistemas",
+        curso: "tecnico em desenvolvimento de sistemas",
+        turma: "3B",
+        professor:"Ramon"
     }
 ]
 
-app.get('/aulas', (req, res) => {
-    fs.readFile('bancoDeDados.json', 'utf-8', (err,data) => {
-        if (err) {
-            res.status(500).json ({msg:"erro no servidor"})
-        }
-        res.status(200).json(JSON.parse(data))
-    })
+function readFile(){
     
-    }) 
+}
 
-app.get('/aulas/:id', (req,res) =>{
-    const id = req.params.id;
+app.get('/aulas', (req,res) => {
+    res.status(200).send(bancodeDados)
+})
+
+app.get('/aulas/:id', (req,res)=>{
+    const id = req.params.id
+    fs.readFile('bancoDeDados.json', 'utf-8', (err, data) =>{
+        if (err) {
+            return res.status(500).send('Erro ao ler o arquivo');
+        }
+        const aulas = JSON.parse(data)
+        const aula = usuarios.find(aula => aula.id == id)
+        if (aula) {
+            return res.status(200).send(aula);
+        } else {
+            return res.status(404).send('Usuário não encontrado');
+        }
+    })
+})
+
+app.post('/aulas', (req, res) => {
+    const dados = req.body
     fs.readFile('bancoDeDados.json', 'utf-8', (err, data) => {
         if(err){
-            res.status(500).json({msg:"erro nos servidor"})
+            return res.status(500).send('Erro ao ler o arquivo');
         }
-        const usuarios = JSON.parse(data)
-        const user = usuarios.find(user => user.id == id)
-        if(user){
-            res.status(200).json(user)
-        }
-        res.status(404).json({msg: 'Usuario nao encontrado'})
+        const aulas = JSON.parse(data)
+        dados['id'] = aulas.length + 1
+        aulas.push(dados)
+        fs.writeFile('bancoDeDados.json', JSON.stringify(aulas), (err) => {
+            if (err) {
+                return res.status(500).send('Erro ao escrever no arquivo');
+            }
+        })
+        console.log(aulas)
     })
+    res.status(201).send(dados)
 })
 
-app.post('/aulas', (req,res) => {
-        const dados =req.body
-        dados['id'] = bancoDeDados.length +1
-        bancoDeDados.push(dados)
-        res.status(201).send(dados)
-    })
-
-app.put('/aulas/:id' , (req, res) => {
-    const id = req.params.id
-    //procurar o id do array
-    const usuario = bancoDeDados.find(user => user.id === id)
-    if(!usuario){
-        res.status(404).json({msg: 'usuario nao encontrado'})
+app.put('/aulas/:id', (req,res)=>{
+    const id = req.params.id;
+    const usuario = bancodeDados.find(user => user.id == id);
+    if (!usuario){
+        return res.status(404).json({msg:"Usuario não encontrado"});
     }
-    res.send('ok')
-})
+    Object.assign(usuario, req.body);
+    res.status(200).json(usuario);
+});
 
-app.delete('/aulas/:id', (req,res) => {
-    const id = req.params.id
-    const userIndex = bancoDeDados.findIndex(user => user.id === id)
-    if(userIndex = -1){
-        res.status(404).json({msg: 'Usuario nao encontrado'})
+
+app.delete('/aulas/:id', (req, res) => {
+    const id = req.params.id;
+    const index = bancodeDados.findIndex(user => user.id == id);
+
+    if (index === -1) {
+        return res.status(404).json({msg: "Usuario não encontrado"});
     }
-    bancoDeDados.splice(userIndex, 1)
-    res.status(204).send()
-})
 
-app.listen(PORT, () => {console.log('servidor online')})  
+    bancodeDados.splice(index, 1);
+    res.status(200).json({msg: "Usuario deletado com sucesso"});
+});
 
-app.get('/aulas', (req,res) => {res.send('aulas de desenvolvimento da 3b')})
-app.post('/aulas', (req,res) => {res.send('criando uma aula')})
-
-
-app.listen(PORT, () => {console.log('servidor online')}) 
+app.listen(PORT, () => {console.log('servidor online')})
